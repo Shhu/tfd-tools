ce406cc08558d7d785dd5f2eb3a9a361250c6968
yann <lowscaa@gmail.com>
2024-08-18 19:11:16 +0200

    wip

---
+++ src/views/AmorphousMaterialListView.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { amorphous } from '@/collections/amorphous';
+
+</script>
+
+<template>
+  <div>
+    <h1>Amorphous Material List</h1>
+
+    <ul>
+      <li
+        v-for="a in amorphous"
+        :key="a.code"
+      >
+        {{ a.code }}
+        {{ a.sourceType }}
+        {{ a.openerType }}
+      </li>
+    </ul>
+  </div>
+</template>
--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,19 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import Unimport from 'unimport/unplugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+      Unimport.vite({
+        dts: true ,
+        dirs: [
+          './composables/*'
+        ]
+      })
   ],
   resolve: {
     alias: {
